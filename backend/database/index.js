86ae5db8436c691eac319ffc5908560d9b907fb4
Cevin Thomas <cevin.thomas.ny@gmail.com>
2019-12-02 14:59:22 +0100

    Database connection now working

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,1 +1,24 @@
-module.exports = console.log( "hello" );
+// Mongodb client
+const MongoClient = require( "mongodb" ).MongoClient;
+
+
+// Connection URL
+const url = "mongodb+srv://CevinThomas:Nygiants1@cluster0-xlcnc.mongodb.net/admin?retryWrites=true&w=majority";
+
+// Database Name
+const dbName = "alliance";
+
+// Create a new MongoClient
+const client = new MongoClient( url );
+
+// Use connect method to connect to the Server
+client.connect( function ( err ) {
+
+    console.log( "Connected successfully to server" );
+
+    const db = client.db( dbName );
+
+    client.close();
+} );
+
+module.exports = client;
