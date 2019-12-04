afe8aafc8095538451198ec0e254f8f8816017d1
Cevin Thomas <cevin.thomas.ny@gmail.com>
2019-12-04 13:24:01 +0100

    Basic login route logic

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,7 +5,9 @@ const jwt = require( "jsonwebtoken" );
 exports.addUser = async ( req, res, next ) => {
     const email = req.body.email;
 
-    if ( User.findUserByEmail( email ) !== null ) {
+    const user = await User.findUserByEmail( email );
+
+    if ( user !== null ) {
         res.status( 200 ).send( "This email already exists" );
     } else {
         const token = jwt.sign( { email }, process.env.JWTSECRET );
@@ -23,5 +25,24 @@ exports.addUser = async ( req, res, next ) => {
 
 exports.login = async ( req, res, next ) => {
     const email = req.body.email;
-    const password = req.body.password;
+    const user = await User.findUserByEmail( email );
+
+    if ( user === null ) {
+        res.status( 200 ).send( "We could not find a user with these credentials" );
+    } else {
+        bcrypt.compare( req.body.password, user.password, ( err, response ) => {
+            if ( err ) {
+                throw err;
+            }
+
+            if ( user.tokens.length !== 0 ) {
+                res.status( 200 ).send( "You are already logged in" );
+            } else {
+                const token = jwt.sign( { email }, process.env.JWTSECRET );
+                user.addTokenToUser( token );
+                res.status( 200 ).send( "You are now logged in" );
+            }
+        } );
+    }
+
 };
