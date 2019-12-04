81c642fa98579937aede75df8672885b3327f805
Cevin Thomas <cevin.thomas.ny@gmail.com>
2019-12-04 13:55:34 +0100

    Move code so new callback function works

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,46 +5,45 @@ const jwt = require( "jsonwebtoken" );
 exports.addUser = async ( req, res, next ) => {
     const email = req.body.email;
 
-    const user = await User.findUserByEmail( email );
+    User.findUserInDatabase( "email", email, ( user ) => {
+        if ( user !== null ) {
+            res.status( 200 ).send( "This email already exists" );
+        } else {
+            const token = jwt.sign( { email }, process.env.JWTSECRET );
 
-    if ( user !== null ) {
-        res.status( 200 ).send( "This email already exists" );
-    } else {
-        const token = jwt.sign( { email }, process.env.JWTSECRET );
-
-        bcrypt.hash( req.body.password, 10, function ( err, hash ) {
-            const user = new User( req.body.name, email, hash );
-            user.saveUser();
-            user.addTokenToUser( token );
-        } );
+            bcrypt.hash( req.body.password, 10, function ( err, hash ) {
+                const user = new User( req.body.name, email, hash );
+                user.saveUser();
+                user.addTokenToUser( token );
+            } );
 
 
-        res.status( 200 ).send( "User succesfully created" );
-    }
+            res.status( 200 ).send( "User succesfully created" );
+        }
+    } );
 };
 
 exports.login = async ( req, res, next ) => {
     const email = req.body.email;
-    const user = await User.findUserByEmail( email );
+    const user = await User.findUserInDatabase( "email", email, ( user ) => {
+        if ( user === null ) {
+            res.status( 200 ).send( "We could not find a user with these credentials" );
+        } else {
+            bcrypt.compare( req.body.password, user.password, ( err, response ) => {
+                if ( err ) {
+                    throw err;
+                }
 
-    if ( user === null ) {
-        res.status( 200 ).send( "We could not find a user with these credentials" );
-    } else {
-        bcrypt.compare( req.body.password, user.password, ( err, response ) => {
-            if ( err ) {
-                throw err;
-            }
-
-            if ( user.tokens.length !== 0 ) {
-                res.status( 200 ).send( "You are already logged in" );
-            } else {
-                const token = jwt.sign( { email }, process.env.JWTSECRET );
-                user.addTokenToUser( token );
-                res.status( 200 ).send( "You are now logged in" );
-            }
-        } );
-    }
-
+                if ( user.tokens.length !== 0 ) {
+                    res.status( 200 ).send( "You are already logged in" );
+                } else {
+                    const token = jwt.sign( { email }, process.env.JWTSECRET );
+                    user.addTokenToUser( token );
+                    res.status( 200 ).send( "You are now logged in" );
+                }
+            } );
+        }
+    } );
 };
 
 exports.logout = async ( req, res, next ) => {
