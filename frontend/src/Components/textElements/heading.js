1e5b04839c555cdbf5000e1224a151b956b68946
Cevin Thomas <cevin.thomas.ny@gmail.com>
2019-12-06 12:26:28 +0100

    Heading conditional rendering

--- frontend/src/Components/textElements/heading.js
+++ frontend/src/Components/textElements/heading.js
@@ -1,8 +1,30 @@
 import React from "react";
 
-const Heading = () => {
-    
-    return <h1>Heading</h1>;
+const Heading = ( props ) => {
+
+    let heading;
+    switch ( props.type ) {
+        case "h2":
+            heading = <h2>{props.title}</h2>;
+            break;
+        case "h3":
+            heading = <h3>{props.title}</h3>;
+            break;
+        case "h4":
+            heading = <h4>{props.title}</h4>;
+            break;
+        case "h5":
+            heading = <h5>{props.title}</h5>;
+            break;
+        default:
+            heading = <h1>{props.title}</h1>;
+    }
+
+    return (
+        <div>
+            {heading}
+        </div>
+    );
 };
 
 export default Heading;
