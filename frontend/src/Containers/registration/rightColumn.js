0519df2b469fd1828720c3707a4abff76dc0870a
Cevin Thomas <cevin.thomas.ny@gmail.com>
2019-12-06 13:43:59 +0100

    Adding more heading and containers and input

--- frontend/src/Containers/registration/rightColumn.js
+++ frontend/src/Containers/registration/rightColumn.js
@@ -2,9 +2,15 @@ import React from "react";
 import Heading from "../../Components/textElements/heading";
 
 const RightColumn = ( props ) => {
+    //TODO: Maybe remake this with a HOC so that the container div is not JSX inside of this container
     return (
         <div id={props.id}>
-            <Heading class={"registration-heading"} title={"Right Column"}/>
+            <div id={"registration-inner"}>
+                <div>
+                    <Heading class={"registration-heading"} title={"Right Column"}/>
+                    <Heading class={"registration-heading"} title={"We need some things from you"}/>
+                </div>
+            </div>
         </div>
     );
 };
