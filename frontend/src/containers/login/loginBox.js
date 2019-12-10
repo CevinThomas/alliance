bd304d8dc8f98b91e9332b6d5f5ea9636429bee9
Cevin Thomas <cevin.thomas.ny@gmail.com>
2019-12-10 12:44:41 +0100

    ErrorMessage styling

--- frontend/src/containers/login/loginBox.js
+++ frontend/src/containers/login/loginBox.js
@@ -4,6 +4,8 @@ import Heading from "../../components/textElements/heading";
 import Input from "../../components/forms/input";
 import Button from "../../components/general/button";
 import Overlay from "../../components/general/overlay";
+import NotMember from "../../components/misc/notMember";
+import ErrorMessage from "../../components/misc/errorMessage";
 
 const LoginBox = ( props ) => {
     return (
@@ -11,11 +13,13 @@ const LoginBox = ( props ) => {
             <Overlay/>
             <AvatarIcon/>
             <Heading class={"login-header"} title={"Login"}/>
+            <ErrorMessage class={"error-div"} errorMessage={"Error test"}/>
             <div className={"form"} id={"loginForm"}>
                 <div id={"form-inner"}>
                     <Input placeholder={"Email"} type={"email"} name={"email"}/>
                     <Input placeholder={"Password"} type={"password"} name={"password"}/>
                     <Button title={"Login"}/>
+                    <NotMember link={"/registration"} message={"Not a member?"} id={"not-login"}/>
                 </div>
             </div>
         </div>
