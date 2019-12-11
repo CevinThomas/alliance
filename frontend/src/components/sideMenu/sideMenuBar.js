80a0b52aad6e247b3df6557513852e180cec0b4b
Cevin Thomas <cevin.thomas.ny@gmail.com>
2019-12-11 11:40:40 +0100

    Adding sidemenu styling

---
+++ frontend/src/components/sideMenu/sideMenuBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
+import OverlayTransition from "../general/overlay-transition";
+
+const SideMenuBar = ( props ) => {
+
+    const links = Object.entries( props.links ).map( ( link ) => {
+        return (
+            <div className={"side-menu-link-container"}>
+                <OverlayTransition/>
+                <Link to={link[1].path}>{link[1].text}</Link>
+            </div>
+        );
+    } );
+
+    return (
+        <React.Fragment>
+            {links}
+        </React.Fragment>
+    );
+};
+
+SideMenuBar.propTypes = {
+    links: PropTypes.object.isRequired
+};
+
+export default SideMenuBar;
--- frontend/src/containers/admin/sideMenu.js
+++ frontend/src/containers/admin/sideMenu.js
@@ -1,10 +1,22 @@
 import React from "react";
-import SideMenuBar from "../../components/sideMenu/sideMenu";
+import SideMenuBar from "../../components/sideMenu/sideMenuBar";
 
 const SideMenu = () => {
+
+    const sideMenuLinks = {
+        linkOne: {
+            path: "/admin/faq",
+            text: "FAQ"
+        },
+        linkTwo: {
+            path: "/admin/guides",
+            text: "Guides"
+        }
+    };
+
     return (
-        <div className={"side-menu"} id={"account-side-menu"}>
-            <SideMenuBar/>
+        <div className={"side-menu"} id={"admin-side-menu"}>
+            <SideMenuBar links={sideMenuLinks}/>
         </div>
     );
 };
