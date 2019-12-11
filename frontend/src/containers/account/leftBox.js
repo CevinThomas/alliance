3bb73444213cee1102aa5479983bdd142d874013
Cevin Thomas <cevin.thomas.ny@gmail.com>
2019-12-11 10:08:21 +0100

    Add link to admin

--- frontend/src/containers/account/leftBox.js
+++ frontend/src/containers/account/leftBox.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import Heading from "../../components/textElements/heading";
 import Button from "../../components/general/button";
 import Paragraph from "../../components/textElements/paragraph";
@@ -6,9 +7,11 @@ import Paragraph from "../../components/textElements/paragraph";
 const LeftBox = () => {
     return (
         <div className={"account-box"}>
-            <Heading type={"h3"} title={"Left Box"}/>
-            <Paragraph title={"Lorem Ipsum"}/>
-            <Button title={"Left Box CTA"}/>
+            <Heading type={"h3"} title={"Admin"}/>
+            <Paragraph title={"Enter the admin area, where you can create Challenge Spaces, or Challenge Tasks"}/>
+            <Link to={"/admin"}>
+                <Button title={"Admin"}/>
+            </Link>
         </div>
     );
 };
