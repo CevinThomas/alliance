471923a5c295460465bdf16ef599e75b1c48025c
Cevin Thomas <cevin.thomas.ny@gmail.com>
2019-12-11 16:53:44 +0100

    Styling for admin box

--- frontend/src/containers/account/rightSection.js
+++ frontend/src/containers/account/rightSection.js
@@ -6,7 +6,14 @@ const RightSection = () => {
     return (
         <div id={"right-section"}>
             <Heading type={"h2"} title={"My Statistics"}/>
-            <Box/>
+            <div className={"right-section-box-container"}>
+                <Box class={"right-section-box"} type={"h3"} title={"Metric 1"}/>
+                <Box class={"right-section-box"} type={"h3"} title={"Metric 2"}/>
+                <Box class={"right-section-box"} type={"h3"} title={"Metric 3"}/>
+                <Box class={"right-section-box"} type={"h3"} title={"Metric 4"}/>
+                <Box class={"right-section-box"} type={"h3"} title={"Metric 5"}/>
+                <Box class={"right-section-box"} type={"h3"} title={"Metric 6"}/>
+            </div>
         </div>
     );
 };
