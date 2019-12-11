9ad1f9e14b72670e01ee1f8be0974142017fab4a
Cevin Thomas <cevin.thomas.ny@gmail.com>
2019-12-11 17:23:56 +0100

    Styling and remove some HTML comments

--- frontend/src/containers/account/leftSection.js
+++ frontend/src/containers/account/leftSection.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Heading from "../../components/textElements/heading";
 import Paragraph from "../../components/textElements/paragraph";
-import Overlay from "../../components/general/overlay";
 
 const LeftSection = () => {
     return (
         <div id={"left-section"}>
-            <Overlay id={"left-section-overlay"}/>
             <Heading type={"h2"} title={"About Me"}/>
             <Paragraph title={"This could be some general information about the user"}/>
             <Paragraph title={"This could be some general information about the user"}/>
