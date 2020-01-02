67f98dce26a0ca5e4673ec7116d3101ad561af77
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-01-02 13:55:13 +0100

    Setting up select logics

--- frontend/src/components/forms/select.js
+++ frontend/src/components/forms/select.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Select = () => {
+const Select = ( props ) => {
+
+    const [ selectedMonth, setSelectedMonth ] = useState( "January" );
+    const [ currentMonthMaxDays, setCurrentMonthMaxDays ] = useState( 31 );
+    console.log( selectedMonth );
+    console.log( currentMonthMaxDays );
+
+    let monthUI;
+    monthUI = Object.entries( props.months ).map( ( month ) => {
+        const removedNumber = month.splice( 1, 1 );
+        return (
+            <option key={removedNumber} value={removedNumber}>
+                {removedNumber}
+            </option>
+        );
+    } );
+
+    let maxDays = [];
+    for ( let i = 1; i < currentMonthMaxDays; i++ ) {
+        maxDays.push( i );
+    }
+
+    let daysUI;
+    daysUI = maxDays.map( ( day ) => {
+        return (
+            <option>
+                {day}
+            </option>
+        );
+    } );
+    
+    const handleMonthChange = ( e ) => {
+        setSelectedMonth( e.target.value );
+        displayCorrectDays( e.target.value );
+    };
+
+    const displayCorrectDays = ( newMonth ) => {
+        Object.entries( props.maxDays ).map( ( day ) => {
+            if ( day[0] === newMonth ) {
+                setCurrentMonthMaxDays( day[1].days );
+            }
+        } );
+    };
+
     return (
-        <div>
-
-        </div>
+        <React.Fragment>
+            <select onChange={handleMonthChange}>
+                {monthUI}
+            </select>
+            <select>
+                {daysUI}
+            </select>
+        </React.Fragment>
     );
 };
 
--- frontend/src/containers/createChallenge/finalFormStep.js
+++ frontend/src/containers/createChallenge/finalFormStep.js
@@ -1,12 +1,29 @@
 import React from "react";
 import {connect} from "react-redux";
 import Input from "../../components/forms/input";
+import Select from "../../components/forms/select";
 
 const mapStateToProps = state => {
     return { chosenTask: state.chosenTaskType };
 };
 
 const FinalFormStep = ( props ) => {
+
+    const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    const maxDays = {
+        January: { days: 31 },
+        February: { days: 28 },
+        March: { days: 31 },
+        April: { days: 30 },
+        May: { days: 31 },
+        June: { days: 30 },
+        July: { days: 31 },
+        August: { days: 31 },
+        September: { days: 30 },
+        October: { days: 31 },
+        November: { days: 30 },
+        December: { days: 31 }
+    };
 
     let formUI;
     //TODO: Remake these into the environment variables just like in redux for easier maintenance
@@ -15,6 +32,9 @@ const FinalFormStep = ( props ) => {
             <div>
                 <Input type={"text"} placeholder={"Name of Challenge"}/>
                 <Input type={"text"} placeholder={"Description of Challenge"}/>
+                <div>
+                    <Select months={months} maxDays={maxDays}/>
+                </div>
             </div>
         );
     } else if ( props.chosenTask === "Single" ) {
