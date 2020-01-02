669b9b6047b246b465918eb06f720e4e3dac6b83
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-01-02 11:13:33 +0100

    Adding the final form setup logic and files

---
+++ frontend/src/containers/createChallenge/finalFormStep.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FinalFormStep = () => {
+    return (
+        <div>
+            <h2>Final form step</h2>
+        </div>
+    );
+};
+
+export default FinalFormStep;
--- frontend/src/views/createTask.js
+++ frontend/src/views/createTask.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ChooseChallenge from "../containers/createChallenge";
 import {connect} from "react-redux";
-import ShowSpaceStep from "../containers/createSpace/showSpaceStep";
+import ShowSpaceStep from "../containers/createChallenge/showSpaceStep";
+import FinalFormStep from "../containers/createChallenge/finalFormStep";
 
 const mapStateToProps = state => {
-    return { chosenTaskType: state.chosenTaskType };
+    return { chosenTaskType: state.chosenTaskType, chosenSpace: state.chosenSpace };
 };
 
 const CreateChallenge = ( props ) => {
 
     let stepsUI;
-    if ( props.chosenTaskType !== "" ) {
+
+    if ( props.chosenSpace !== "" && props.chosenTaskType !== "" ) {
+        stepsUI = <FinalFormStep/>;
+    } else if ( props.chosenTaskType !== "" ) {
         stepsUI = <ShowSpaceStep/>;
     } else {
         stepsUI = <ChooseChallenge/>;
