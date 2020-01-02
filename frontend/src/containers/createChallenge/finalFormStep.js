783f8656c729ae8ebc54ef14a704ef5e55eb688f
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-01-02 11:27:07 +0100

    Setting up form

--- frontend/src/containers/createChallenge/finalFormStep.js
+++ frontend/src/containers/createChallenge/finalFormStep.js
@@ -1,11 +1,38 @@
 import React from "react";
+import {connect} from "react-redux";
+import Input from "../../components/forms/input";
 
-const FinalFormStep = () => {
+const mapStateToProps = state => {
+    return { chosenTask: state.chosenTaskType };
+};
+
+const FinalFormStep = ( props ) => {
+
+    let formUI;
+    //TODO: Remake these into the environment variables just like in redux for easier maintenance
+    if ( props.chosenTask === "Checkbox" ) {
+        formUI = (
+            <div>
+                <Input placeholder={"Name of Challenge"}/>
+            </div>
+        );
+    } else if ( props.chosenTask === "Single" ) {
+
+    } else if ( props.chosenTask === "Big" ) {
+
+    } else {
+
+    }
+
     return (
         <div>
             <h2>Final form step</h2>
+            <div>
+                <h2>You have chosen {props.chosenTask}</h2>
+                {formUI}
+            </div>
         </div>
     );
 };
 
-export default FinalFormStep;
+export default connect( mapStateToProps )( FinalFormStep );
