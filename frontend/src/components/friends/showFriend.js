95528296a2f53907f7a033b4faa95905efacc6b5
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-01-10 09:50:29 +0100

    Begin off CSS button friend

--- frontend/src/components/friends/showFriend.js
+++ frontend/src/components/friends/showFriend.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {connect} from "react-redux";
 import Heading from "../textElements/heading";
+import Button from "../general/button";
 
 const mapStateToProps = state => {
     return { friend: state.viewFriend };
@@ -9,6 +10,7 @@ const mapStateToProps = state => {
 const ShowFriend = ( props ) => {
     return (
         <div id={props.id}>
+            <Button id={"friend-back"} title={"Go Back"}/>
             <Heading id={"main-heading"} title={"Friend " + props.friend.name} type={"h1"}/>
         </div>
     );
