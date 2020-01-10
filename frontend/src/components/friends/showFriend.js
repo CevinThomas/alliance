4ba332151f98eef6a3a77db7c21cd922c9e52785
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-01-10 10:01:59 +0100

    Go back function implemented

--- frontend/src/components/friends/showFriend.js
+++ frontend/src/components/friends/showFriend.js
@@ -2,18 +2,25 @@ import React from "react";
 import {connect} from "react-redux";
 import Heading from "../textElements/heading";
 import Button from "../general/button";
+import {withRouter} from "react-router";
+
 
 const mapStateToProps = state => {
     return { friend: state.viewFriend };
 };
 
 const ShowFriend = ( props ) => {
+
+    const goBackButtonClickHandler = () => {
+        props.history.goBack();
+    };
+
     return (
         <div id={props.id}>
-            <Button id={"friend-back"} title={"Go Back"}/>
+            <Button onclick={goBackButtonClickHandler} id={"friend-back"} title={"Go Back"}/>
             <Heading id={"main-heading"} title={"Friend " + props.friend.name} type={"h1"}/>
         </div>
     );
 };
 
-export default connect( mapStateToProps )( ShowFriend );
+export default connect( mapStateToProps )( withRouter( ShowFriend ) );
