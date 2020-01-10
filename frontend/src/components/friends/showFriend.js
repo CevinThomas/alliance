01390a51d532759b414ed23b8d7e1b2cea5d7f36
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-01-10 10:28:53 +0100

    Frontend friend account

--- frontend/src/components/friends/showFriend.js
+++ frontend/src/components/friends/showFriend.js
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 import Heading from "../textElements/heading";
 import Button from "../general/button";
 import {withRouter} from "react-router";
+import Paragraph from "../textElements/paragraph";
 
 
 const mapStateToProps = state => {
@@ -19,6 +20,20 @@ const ShowFriend = ( props ) => {
         <div id={props.id}>
             <Button onclick={goBackButtonClickHandler} id={"friend-back"} title={"Go Back"}/>
             <Heading id={"main-heading"} title={"Friend " + props.friend.name} type={"h1"}/>
+            <div id={"friend-information-container"}>
+                <div id={"friend-inner-container"}>
+                    <div id={"friend-information-left"}>
+                        <div id={"friend-image"}></div>
+                    </div>
+                    <div id={"friend-information-right"}>
+                        <div id={"friend-details"}>
+                            <Heading title={"Friend header"} type={"h3"}/>
+                            <Paragraph title={"Stockholm, Tyresö"}/>
+                            <Paragraph title={"hhasuhdayhsdye hadsyayncyen"}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
