9a04e7f9bc81866aa0dfec2fca34baf976091243
Cevin Thomas <cevin.thomas.ny@gmail.com>
2020-01-13 09:42:20 +0100

    Basic structure for space card

--- frontend/src/components/spaces/spaceCard.js
+++ frontend/src/components/spaces/spaceCard.js
@@ -1,13 +1,21 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import Heading from "../textElements/heading";
+import Paragraph from "../textElements/paragraph";
 
 const SpaceCard = ( props ) => {
     return (
-        <div id={props.id} className={"space-card"}>
-            <div className={"space-card-image"}></div>
-            <div>
-
+        <Link to={"/spaces/?id="}>
+            <div id={props.id} className={"space-card"}>
+                <div className={"space-card-image"}></div>
+                <div className={"space-card-text-container"}>
+                    <div className={"space-card-text-inner"}>
+                        <Heading title={"Space"} type={"h3"}/>
+                        <Paragraph title={"Members"}/>
+                    </div>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
